4711ba19c25439df1009068ec1b1e629fd30b9d6
Emmanuel Wooten <ewooten@alumni.emory.edu>
2024-02-02 08:23:57 -0500

    Modified AmmoPickup.cs to play SFX

--- Assets/1. Zombie Runner/Scripts/AmmoPickup.cs
+++ Assets/1. Zombie Runner/Scripts/AmmoPickup.cs
@@ -6,11 +6,20 @@ public class AmmoPickup : MonoBehaviour
 {
     [SerializeField] int ammoAmount = 5;
     [SerializeField] AmmoType ammoType;
+    [SerializeField] AudioSource pickupSound;
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             FindObjectOfType<Ammo>().IncreaseCurrentAmmo(ammoType, ammoAmount);
+
+            // Play the pickup sound effect when the player picks up ammo
+            if (pickupSound != null && !pickupSound.isPlaying)
+            {
+                pickupSound.Play();
+            }
+
             Destroy(gameObject);
         }
     }
