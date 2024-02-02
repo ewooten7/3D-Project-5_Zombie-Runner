ff4d1a63793701ee8898b2513557ff2e7a3d4239
Emmanuel Wooten <ewooten@alumni.emory.edu>
2024-02-02 07:13:07 -0500

    Improved SFX for zombies running; removed redundant changes to EnemyHealth.cs

--- Assets/1. Zombie Runner/Scripts/EnemyHealth.cs
+++ Assets/1. Zombie Runner/Scripts/EnemyHealth.cs
@@ -5,16 +5,8 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] float hitPoints = 100f;
-    private bool isDead = false;
 
-    // Reference to the AudioSource component for running sound
-    private AudioSource runningSound;
-
-    // Add a reference to the running sound AudioSource component
-    void Start()
-    {
-        runningSound = GetComponent<AudioSource>();
-    }
+    bool isDead = false;
 
     public bool IsDead()
     {
@@ -25,7 +17,7 @@ public class EnemyHealth : MonoBehaviour
     {
         BroadcastMessage("OnDamageTaken");
         hitPoints -= damage;
-        if (hitPoints <= 0 && !isDead)
+        if (hitPoints <= 0)
         {
             Die();
         }
@@ -34,13 +26,6 @@ public class EnemyHealth : MonoBehaviour
     private void Die()
     {
         if (isDead) return;
-
-        // Stop the running sound when the enemy dies
-        if (runningSound != null)
-        {
-            runningSound.Stop();
-        }
-
         isDead = true;
         GetComponent<Animator>().SetTrigger("die");
     }
